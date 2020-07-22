1b370d0cff31c73e51e4be6b1ad88ffc10a342aa
Ed. Gi <31946346+dwaxgio@users.noreply.github.com>
2020-07-21 22:42:41 -0500

    Serialization

---
+++ 20_SERIALIZATION/Consola.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20_SERIALIZATION
+{
+    // 1. Definir clase a serializar
+    [Serializable] // Atributo para serializar
+    public class Consola
+    {
+        public int ID;
+        public string Nombre;
+    }
+}
--- 20_SERIALIZATION/Program.cs
+++ 20_SERIALIZATION/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace _20_SERIALIZATION
 {
@@ -6,7 +9,31 @@ namespace _20_SERIALIZATION
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 2. Crear objeto de la clase
+            Consola nuevaConsola = new Consola();
+            nuevaConsola.ID = 1;
+            nuevaConsola.Nombre = "Xbox";
+
+            // 3. Crear el stream del archivo
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(@"D:\CURSO_C#_3_GURU99\CsharpTutorial\20_SERIALIZATION\Archivo.txt", FileMode.Create, FileAccess.Write);
+
+            // 4. Serializar el objeto
+            formatter.Serialize(stream, nuevaConsola);
+            stream.Close();
+
+            // 5. Se crea archivo stream para deserializar
+            stream = new FileStream(@"D:\CURSO_C#_3_GURU99\CsharpTutorial\20_SERIALIZATION\Archivo.txt", FileMode.Open, FileAccess.Read);
+
+            // 6. Se deserializa el objeto
+            Consola nuevaConsola2 = (Consola)formatter.Deserialize(stream);
+
+            // 7. Se escribe la informaciòn en consola
+            Console.WriteLine(nuevaConsola2.ID);
+            Console.WriteLine(nuevaConsola2.Nombre);
+
+            Console.ReadKey();
+
         }
     }
 }
